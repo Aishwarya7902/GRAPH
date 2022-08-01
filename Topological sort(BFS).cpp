c3bb7f207b8e06d0c3f60514f5d2b36f7b8c2d97
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-08-01 16:37:07 +0530

    Update Topological sort(BFS).cpp

--- Topological sort(BFS).cpp
+++ Topological sort(BFS).cpp
@@ -12,7 +12,7 @@ create  a queue ,q
 while(!q.empty()) {
 3.ek temp variable me q ke front ko store kar lo...pop()
 4.print /ans me add kar lo
-5.is vertex se jitne v connected edges hai unko indegree ko reduce karo by 1 .....if it also comes to be 0 then push into queue
+5.is vertex se jitne v connected edges hai unko indegree ko reduce karo by 1 (ISKE PARENT KO GRAPH SE HATA RAHE SO INDEGREE KO 1 KAM KAR RAHE)  .....if it also comes to be 0 then push into queue
 }
 6.at last return ans vector
 
@@ -59,5 +59,84 @@ vector<int> topoSort(int V, vector<int> adj[])
    return ans; 
 }
 };
+
+/*APPROACH 2 :USING STACK
+VIDEO:  https://youtu.be/tv8yJLKOZ0g
+*/
+
+//DFS IMPLEMENTATION
+
+class solution
+
+{
+
+public:
+//Function to return list containing vertices in Topological order.
+void DFS(vector<int> adj[],vector<bool>&visited,int s,stack<int>&st){
+    visited[s]=true;
+    for(int v:adj[s]){
+        if(!visited[v]){
+            DFS(adj,visited,v,st);
+        }
+    }
+    st.push(s);
+    
+}
+vector<int> topoSort(int V, vector<int> adj[]) 
+{
+    // code here
+ stack<int>st;
+ vector<bool>visited(V,false);
+ for(int i=0;i<V;i++){
+     if(!visited[i]){
+         DFS(adj,visited,i,st);
+     }
+ }
+ vector<int>ans;
+ while(!st.empty()){
+     int x=st.top();
+     st.pop();
+     ans.push_back(x);
+ }
+ return ans;
+}
+
+};
+
+/*APPROACH 3: VECTOR ME HI PUSH KARO AND LAST ME VECTOR KO REVERSE KAR DO
+VIDEO:  https://youtu.be/tv8yJLKOZ0g
+*/
+class solution
+
+{
+	public:
+	//Function to return list containing vertices in Topological order.
+	void DFS(vector<int> adj[],vector<bool>&visited,int s,vector<int>&ans){
+	    visited[s]=true;
+	    for(int v:adj[s]){
+	        if(!visited[v]){
+	            DFS(adj,visited,v,ans);
+	        }
+	    }
+	    ans.push_back(s);
+	    
+	}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	
+	 vector<bool>visited(V,false);
+	 vector<int>ans;
+	 for(int i=0;i<V;i++){
+	     if(!visited[i]){
+	         DFS(adj,visited,i,ans);
+	     }
+	 }
+	 reverse(ans.begin(),ans.end());
+	 return ans;
+	}
+};
+
+
 
 
