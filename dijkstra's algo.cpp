b7a460e29d3a9e685fa9e655b91264fae7ddf698
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-09-13 09:30:04 -0700

    Update dijkstra's algo.cpp

--- dijkstra's algo.cpp
+++ dijkstra's algo.cpp
@@ -1,2 +1,13 @@
 FEEL ❤❤💕💕💕💕 : MINIMUM DISTACE WALE KO SABSE PAHLE UTHAO AND DEKHTE CHALE JAO KI KYA TUM KUCH BETTER CONTRIBUTION KAR SAKTE HO.....IF HAN THEN PROIRITY QUEUE ME DAL 
 DO AND REPEAT THIS UNTILL THE PQ IS EMPTY
+
+VIDEO: https://www.youtube.com/watch?v=jbhuqIASjoM&t=491s
+
+TC: O((N+E)logN)
+            /\ this is for priority queue
+    
+SC: O(N ) distance array + O(N) priority queue
+for better understanding of tc and sc visit : https://youtu.be/jbhuqIASjoM?t=943
+
+
+
