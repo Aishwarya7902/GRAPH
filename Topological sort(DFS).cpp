df827a12162e52a5f80eaa89283fb92586848a2a
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-09-26 01:05:18 -0700

    Update Topological sort(DFS).cpp

--- Topological sort(DFS).cpp
+++ Topological sort(DFS).cpp
@@ -1,78 +1,45 @@
 PROBLEM:     https://practice.geeksforgeeks.org/problems/topological-sort/1
 /*APPROACH 1 :USING STACK
 VIDEO:  https://youtu.be/tv8yJLKOZ0g
+        https://www.youtube.com/watch?v=Yh6EFazXipA (striver)
+	intution ❤❤❤❤❤
+	from striver bhaiya's video: since every node will travel it's adjacent first and then at last it will push itself in the stack
+	now for  damn sure ....this will insure that for evry u->v....u will come first in topo sort   as first v will be pushed in stack then u will...hence 
+	u will be popped out first and hence it will be printed before v.
 */
 
 //DFS IMPLEMENTATION
-
-class solution
-
-{
-
-public:
-//Function to return list containing vertices in Topological order.
-void DFS(vector<int> adj[],vector<bool>&visited,int s,stack<int>&st){
-    visited[s]=true;
-    for(int v:adj[s]){
-        if(!visited[v]){
-            DFS(adj,visited,v,st);
-        }
-    }
-    st.push(s);
-    
-}
-vector<int> topoSort(int V, vector<int> adj[]) 
-{
-    // code here
- stack<int>st;
- vector<bool>visited(V,false);
- for(int i=0;i<V;i++){
-     if(!visited[i]){
-         DFS(adj,visited,i,st);
-     }
- }
- vector<int>ans;
- while(!st.empty()){
-     int x=st.top();
-     st.pop();
-     ans.push_back(x);
- }
- return ans;
-}
-
-};
-
-/*APPROACH 2: VECTOR ME HI PUSH KARO AND LAST ME VECTOR KO REVERSE KAR DO
-VIDEO:  https://youtu.be/tv8yJLKOZ0g
-*/
-class solution
-
-{
+class Solution
+{   private:
+   void dfsTopo(int node,vector<int>&vis,stack<int>&st,vector<int> adj[]){
+       vis[node]=1;
+       for(auto it:adj[node])
+       {
+        if(!vis[it]){
+            dfsTopo(it,vis,st,adj);
+        }   
+       }
+       st.push(node);
+   }
 	public:
-	//Function to return list containing vertices in Topological order.
-	void DFS(vector<int> adj[],vector<bool>&visited,int s,vector<int>&ans){
-	    visited[s]=true;
-	    for(int v:adj[s]){
-	        if(!visited[v]){
-	            DFS(adj,visited,v,ans);
-	        }
-	    }
-	    ans.push_back(s);
-	    
-	}
-	vector<int> topoSort(int V, vector<int> adj[]) 
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V,vector<int> adj[] ) 
 	{
-	    // code here
-	
-	 vector<bool>visited(V,false);
-	 vector<int>ans;
-	 for(int i=0;i<V;i++){
-	     if(!visited[i]){
-	         DFS(adj,visited,i,ans);
-	     }
+	    // code here 
+	  vector<int> vis(V,0);
+	  stack<int>st;
+	  for(int i=0;i<V;i++){
+	      if(!vis[i]){
+	          dfsTopo(i,vis,st,adj);
+	      }
+	  }
+	  
+	 vector<int>topo;
+	 while(!st.empty()){
+	     topo.push_back(st.top());
+	     st.pop();
 	 }
-	 reverse(ans.begin(),ans.end());
-	 return ans;
+	 return topo;
 	}
 };
 
